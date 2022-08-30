5734b055fdb365b538607c468fc99629c919a8b4
EVELYNLUCCA <eve-lanegra@hotmail.com>
2022-08-30 02:36:12 -0300

    cambio de url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  // apiURL: 'https://backend-ap-eve.herokuapp.com'
-  apiURL: 'http://localhost:8080'
+  apiURL: 'https://backend-ap-eve.herokuapp.com'
+  // apiURL: 'http://localhost:8080'
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  // apiURL: 'https://backend-ap-eve.herokuapp.com',
-  apiURL: 'http://localhost:8080'
+  apiURL: 'https://backend-ap-eve.herokuapp.com',
+  // apiURL: 'http://localhost:8080'
 };
 
 /*
